6c4e44f7ce8c17bd4f2e1bfb98732da6e4a3f1a6
Yann <yann@minimaldesign.net>
2024-05-02 22:42:53 -0700

    + postcss-preset-env to use custom media queries
    Also, started adding media queries from open-props

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,18 @@
-module.exports = {
-  plugins: [require("autoprefixer")],
+const autoprefixer = require("autoprefixer");
+const postcssPresetEnv = require("postcss-preset-env");
+
+const config = {
+  plugins: [
+    postcssPresetEnv({
+      stage: 3,
+      features: {
+        "nesting-rules": true,
+        "custom-media-queries": true,
+        // "media-query-ranges": true,
+      },
+    }),
+    autoprefixer(),
+  ],
 };
+
+module.exports = config;
