6a10b1d3b258f0c5db32f3f3e7cc93c1a37fa1cf
Yann <yann@minimaldesign.net>
2024-07-01 12:33:41 -0700

    update RSS feed to use the blog's getCollection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
+  const blog = await getCollection("blog");
   return rss({
     title: "mCSS News",
     description: "Stay up to date with mCSS updates",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
