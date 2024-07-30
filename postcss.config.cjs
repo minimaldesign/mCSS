79b3389bef24483098f6d3d285da5f749340e540
Yann <yann@minimaldesign.net>
2024-07-30 16:35:27 -0700

    remove autoprefixer because it's included in postcssPresetEnv

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,17 +1,10 @@
-const autoprefixer = require("autoprefixer");
 const postcssPresetEnv = require("postcss-preset-env");
 
 const config = {
   plugins: [
     postcssPresetEnv({
-      stage: 3,
-      features: {
-        "nesting-rules": true,
-        "custom-media-queries": true,
-        // "media-query-ranges": true,
-      },
+      stage: 2,
     }),
-    autoprefixer(),
   ],
 };
 
