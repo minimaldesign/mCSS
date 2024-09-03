b0b7109b4134c48ef28a00f3642becaba6f36b93
Yann <yann@minimaldesign.net>
2024-09-02 20:40:11 -0700

    Move "Getting Started" to the top of the sidebar nav

--- src/scripts/utilities.js
+++ src/scripts/utilities.js
@@ -18,3 +18,17 @@ export function throttle(func, delay) {
     }
   };
 }
+
+export function moveToFirstPosition(arr, key, value) {
+  // Find the index of the object that matches the key-value pair
+  const index = arr.findIndex((obj) => obj[key] === value);
+  // If a match is found
+  if (index !== -1) {
+    // Remove the object from its current position
+    const [matchedObject] = arr.splice(index, 1);
+    // Add the matched object to the beginning of the array
+    arr.unshift(matchedObject);
+  }
+  // Return the modified array
+  return arr;
+}
