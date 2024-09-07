cb3b51109b000a31ce84b85db302c1e5c35aa73c
Yann <yann@minimaldesign.net>
2024-09-06 19:27:15 -0700

    add astro-purgecss integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
-
 import mdx from "@astrojs/mdx";
+
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mcss.dev",
-  integrations: [preact(), mdx()]
+  integrations: [preact(), mdx(), purgecss()]
 });
