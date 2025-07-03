9c1c177b9f3f27d8ef88cae4047864b7b600b2ad
Yann <yann@minimaldesign.net>
2025-07-03 14:16:04 -0700

    Add astro-tunnel to expose local dev to the web for dev 🎉

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import mdx from "@astrojs/mdx";
 
+import tunnel from "astro-tunnel";
+
 // import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mcss.dev",
-  integrations: [preact(), mdx()],
+  integrations: [preact(), mdx(), tunnel()],
 });
