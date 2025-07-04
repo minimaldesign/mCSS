80dacd3dca82160afafae6df6081625e3b5977a6
Yann <yann@minimaldesign.net>
2025-07-03 21:01:46 -0700

    Added sentry's spotlightjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,19 @@ import mdx from "@astrojs/mdx";
 
 import tunnel from "astro-tunnel";
 
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
+
 // import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://mcss.dev",
-  integrations: [preact(), mdx(), tunnel()],
+  integrations: [
+    preact(),
+    mdx(),
+    tunnel(),
+    sentry(),
+    spotlightjs(),
+  ],
 });
